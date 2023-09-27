441b0851608221d36abdac9927a876e0655d71f4
King Fai Aaron <kingfai.977@gmail.com>
2023-09-27 16:53:31 +0800

    First Commit

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./dist/output.css" rel="stylesheet">
+    <link rel="stylesheet" href="./Assets/css/style.css">
+    <title>King Fai Aaron | Web Developer</title>
+</head>
+
+<body>
+    <nav class="bg-transparent">
+        <div class="flex justify-center content-center py-5 px-8 m-auto max-w-screen-2xl items-center">
+            <div class="w-2/4 text-slate-900 text-2xl my-3">
+                KING
+            </div>
+            <div class="menus w-2/4">
+                <ul class="list-none flex justify-evenly no-underline text-slate-900 my-3 items-center">
+                    <li><a class="hover:bg-slate-900 hover:text-white hover:shadow-slate-900 hover:font-black py-2 px-5 mx-1 hover:border-solid transition-all duration-300 hover:rounded-lg" href="#home">Home</a></li>
+                    <li><a class="hover:bg-slate-900 hover:text-white hover:shadow-slate-900 hover:font-black py-2 px-5 mx-1 hover:border-solid transition-all duration-300 hover:rounded-lg" href="#services">Services</a></li>
+                    <li><a class="hover:bg-slate-900 hover:text-white hover:shadow-slate-900 hover:font-black py-2 px-5 mx-1 hover:border-solid transition-all duration-300 hover:rounded-lg" href="#stacks">Stacks</a></li>
+                    <li><a class="hover:bg-slate-900 hover:text-white hover:shadow-slate-900 hover:font-black py-2 px-5 mx-1 hover:border-solid transition-all duration-300 hover:rounded-lg" href="#skills">Skills</a></li>
+                    <li><a class="hover:bg-slate-900 hover:text-white hover:shadow-slate-900 hover:font-black py-2 px-5 mx-1 hover:border-solid transition-all duration-300 hover:rounded-lg" href="#portfolio">Portfolio</a></li>
+                    <li><a class="hover:bg-slate-900 hover:text-white hover:shadow-slate-900 hover:font-black py-2 px-5 mx-1 hover:border-solid transition-all duration-300 hover:rounded-lg" href="#testimonial">Testimoinial</a></li>
+                    <li><a class="hover:bg-slate-500 hover:shadow-white hover:font-black py-2 px-5 mx-1 hover:border-solid transition-all duration-300 rounded-lg bg-slate-900 text-white" href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section id="home" class="p-16 hero">
+        <div class="flex justify-center m-auto align-middle flex-wrap content-center h-95">
+            <div class="max-w-screen-2xl text-center">
+                <h1 class="text-8xl text-center font-bold uppercase text-slate-900 stroke-yellow-300 ">lets take your business into another level</h1>
+                <p class="text-xl text-slate-900 capitalize">king fai aaron | web developer</p>
+                <div class="social-media-icons flex max-w-md justify-center flex-wrap content-center mx-auto my-5 gap-5">
+                    <a href="#" target="_blank"><img src="./Assets/images/facebook-icon.png" class="w-6" alt="Facebook" width="150px"></a>
+                    <a href="#" target="_blank"><img src="./Assets/images/linkedin-icon.png" class="w-6" alt="LinkedIn" width="150px"></a>
+                    <a href="#" target="_blank"><img src="./Assets/images/github-icon.png" class="w-6" alt="GitHub" width="150px"></a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="achivements" class="p-16">
+        <div class="flex -mt-36 justify-center uppercase m-auto text-center font-semibold text-white align-middle flex-wrap content-center max-w-screen-2xl">
+            <div class="w-1/4">
+                <div class="mx-3 bg-sky-950/80 py-12 backdrop-blur-lg rounded-3xl drop-shadow-lg">
+                    <p class="text-8xl font-normal">125</p>
+                    Completed Projects
+                </div>
+            </div>
+            <div class="w-1/4">
+                <div class="mx-3 bg-sky-950/80 py-12 backdrop-blur-lg rounded-3xl drop-shadow-lg">
+                    <p class="text-8xl font-normal">12</p>
+                    clients
+                </div>
+            </div>
+            <div class="w-1/4">
+                <div class="mx-3 bg-sky-950/80 py-12 backdrop-blur-lg rounded-3xl drop-shadow-lg">
+                    <p class="text-8xl font-normal">4</p>
+                    years of experience
+                </div>
+            </div>
+            <div class="w-1/4">
+                <div class="mx-3 bg-sky-950/80 py-12 backdrop-blur-lg rounded-3xl drop-shadow-lg">
+                    <p class="text-8xl font-normal">352</p>
+                    Landing Pages
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './components/**/*.{html,js,php}',
+    './pages/**/*.{html,js,php}',
+    './index.php',
+  ],
+  // ...
+}
