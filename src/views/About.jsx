dc4a926bd8a795ce3b571f44b00c3b9d001a4c1a
King Fai Aaron <kingfai.977@gmail.com>
2023-12-11 18:42:08 +0800

    New Update Dec 11 2023

--- src/views/About.jsx
+++ src/views/About.jsx
@@ -1,22 +1,32 @@
-import Mystacks from "../components/MyStacks";
 const About = () => {
   return (
-    <div className="h-90vh max-w-7xl m-auto px-11 flex flex-col justify-center">
+    <div className="mt-[200px] max-w-7xl m-auto px-11 flex flex-col justify-center">
       <h1 className="text-9xl text-white">About.</h1>
       <div className="ms-12 text-white mt-10">
-        <p className="font-bold text-3xl leading-[110%]">Welcome to my corner of digital universe! I&apos;m King, a passionate Front End Developer with keen eye for creating visually appealing and user-friendly websites.</p>
+        <p className="font-bold text-2xl leading-[110%]">Welcome to my corner of digital universe! I&apos;m King, a passionate Front End Developer with keen eye for creating visually appealing and user-friendly websites.</p>
       </div>
-      <div className="flex mt-10 gap-10 text-white text-xl leading-[1.75rem]">
+      <div className="flex mt-14 gap-16 text-white leading-[1.75rem]">
         <div className="w-[35%]">
-          <p className="font-bold text-2xl">Let&apos;s Build Something Amazing:</p>
-          <p className="mt-3">Whether you&apos;re looking to revamp your existing website, launch a new project, or dive into the world of e-commerce, I&apos;m here to help. Let&apos;s collaborate and turn your digital dreams into a stunning reality.</p>
+          <p className="font-bold text-xl">Let&apos;s Build Something Amazing:</p>
+          <p className="mt-3 text-justify">Whether you&apos;re looking to revamp your existing website, launch a new project, or dive into the world of e-commerce, I&apos;m here to help. Let&apos;s collaborate and turn your digital dreams into a stunning reality.</p>
         </div>
-        <div className="w-[65%]">
-          <p className="">
+        <div className="w-[65%] text-justify">
+        <p className="font-bold text-xl mb-2">Philosophy</p>
+          <p className="mb-6">
             My approach to development is rooted in a fusion of creativity and functionality. I believe that a website should not only look good but also perform exceptionally well. Every line of code I write is a piece of a larger puzzle, contributing to a digital landscape that is both
             aesthetically pleasing and highly functional.
           </p>
-          <Mystacks />
+          <p className="font-bold text-xl mb-2">What I do?</p>
+          <p className="mb-6">
+           Back in 2014 during my college, I started to have interest on Web Development and I created my first potfolio on notepad Learning the basic skills. Before I graduated on college I got an award a Best Thesis and started to Learn and get Certificates on
+           <b>SoloLearn</b>. Fast-forward to today, I&apos;ve had the privillege of building Websites for <b>Digital Marketing Agency,</b> a <b>Huge Corporation</b>.
+          </p>
+          <p className="mb-6">
+            My main focus these days is building projects, creating web application using <b>MERN Stack</b> to showcase my skills. In my free time I&apos;ve also Joined on a community on Discord which is <b>Acdemind</b> which collaborating with another developer to build and to learn with them.
+          </p>
+          <p className="mb-6">
+            When I&apos;m not at the computer, I&apos;m usually taking some rest, planning what will be my next step on my life, hanging out with my Girlfriend or Visiting my siblings on my grandparents.
+          </p>
         </div>
       </div>
     </div>
