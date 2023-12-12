0f7bc87d58512c6f443d5923f44f467e7c2adfed
King Fai Aaron <kingfai.977@gmail.com>
2023-12-13 00:18:39 +0800

    Dec 13 2023
    -Added Experience Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ import Portfolio from "./views/Portfolio";
 import Skills from "./views/Skills";
 import Contact from "./views/Contact";
 import About from "./views/About";
+import Experience from "./views/Experience";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Portfolio />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Portfolio />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/experience" element={<Experience />} />
       </Routes>
     </>
   );
---
+++ src/views/Experience.jsx
@@ -0,0 +1,45 @@
+const Experience = () => {
+  return (
+    <div className="mt-[200px] max-w-7xl m-auto px-11 flex flex-col justify-center">
+      <h1 className="text-9xl text-white">Experience.</h1>
+      <div className="ms-12 text-white mt-10">
+        <p className="font-bold text-2xl leading-11">As I continue to evolve in my career, I remain committed to pushing the boundaries of web development. Each experience has been a building block in my journey, and I&apos;m excited about the opportunities and challenges that lie ahead.</p>
+      </div>
+      <div className="flex ms-12 gap-2 mt-10 justify-start text-white">
+        <div className="w-1/12">
+          <p className="opacity-50 text-sm">2021 - Present</p>
+        </div>
+        <div className="w-1/3">
+          <p className="text-custom text-xl font-semibold">Front End Developer · NEO360</p>
+          <div className="flex"></div>
+          <p className="text-5xl leading-[110%] mb-2"></p>
+          <p className="">In my current role at NEO360, I continue to push the boundaries of Front End Development. Focused on creating seamless user experiences, I leverage cutting-edge technologies such as WordPress and Shopify to build modern and responsive interfaces. Collaborating with a forward-thinking team, I contribute to projects that redefine digital experiences and elevate brand identities.</p>
+        </div>
+      </div>
+      <div className="flex ms-12 gap-2 mt-10 justify-start text-white">
+        <div className="w-1/12">
+          <p className="opacity-50 text-sm">2020 - 2021</p>
+        </div>
+        <div className="w-1/3">
+          <p className="text-custom text-xl font-semibold">Laravel Developer · TheRobotExchange</p>
+          <div className="flex"></div>
+          <p className="text-5xl leading-[110%] mb-2"></p>
+          <p className="">Embracing the world of backend development, I transitioned into the role of a Laravel Developer at TheRobotExchange. Here, I delved into the intricacies of building robust and scalable web applications. Working with the Laravel framework, I developed a deep understanding of server-side scripting and database management. This role expanded my skill set and provided a holistic perspective on web development.</p>
+        </div>
+      </div>
+      <div className="flex ms-12 gap-2 my-10 justify-start text-white">
+        <div className="w-1/12">
+          <p className="opacity-50 text-sm">2019 - 2020</p>
+        </div>
+        <div className="w-1/3">
+          <p className="text-custom text-xl font-semibold">Front End Developer · IFormatLogic</p>
+          <div className="flex"></div>
+          <p className="text-5xl leading-[110%] mb-2"></p>
+          <p className="">During my tenure at I FormatLogic, I honed my skills as a Front End Developer, working on projects that demanded precision and creativity. Collaborating with a talented team, I contributed to the development of visually stunning and functionally robust websites. This experience laid the foundation for my journey in the dynamic field of web development.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Experience;
